43636b271c04f31e041d0f74e9447b5e322bfe1c
EnzoPego <enzzo2410@gmail.com>
2023-03-23 19:27:51 -0300

    Cambié la forma en que se obtienen las imágenes para que se tomen de un archivo local en lugar de internet

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css'
 import Navbar from './components/NavBar/NavBar'
 import Header from "./components/Header/Header";
 import CardProduct from "./components/CardProduct/CardProduct";
+import img from '../src/components/CardProduct/CardImg/Img'
+
 
 
 
@@ -18,17 +20,17 @@ class App extends Component {
                     <CardProduct
                         name="Producto 1"
                         info="Descripción_1"
-                        img="https://http2.mlstatic.com/D_NQ_NP_905374-MLU54103550016_032023-O.webp"
+                        img={img.iphone}
                     />
                     <CardProduct
                         name="Producto 2"
                         info="Descripción_2"
-                        img="https://http2.mlstatic.com/D_NQ_NP_864844-MLM51559388062_092022-O.webp"
+                        img={img.xiaomi}
                     />
                     <CardProduct
                         name="Producto 3"
                         info="Descripción_3"
-                        img="https://http2.mlstatic.com/D_NQ_NP_657865-MLA52594819321_112022-O.webp"
+                        img ={img.s23}
                     />
                 </div>
             </div>
